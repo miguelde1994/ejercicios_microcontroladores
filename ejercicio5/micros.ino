ab1e5dd5796f014bb5a91ba227efa1c1a7adc4af
Miguel Dagraza <migueldagraza@gmail.com>
2024-11-27 08:24:36 +0100

    cambio nombres

---
+++ ejercicio5/micros.ino
@@ -0,0 +1,23 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  
+}
+
+void loop() {
+ 
+
+}
+
+
+
+
+/*
+echo 
+a. Recibir datos por la conexión serial.
+b. Reenviar por dicha conexión los datos recibidos cambiando las 
+mayúsculas por minúsculas y las minúsculas por mayúsculas.
+*/
--- ejercicio6/echo.ino
+++ ejercicio6/echo.ino
@@ -48,8 +48,17 @@ void loop() {
 
 
 /*
-echo 
-a. Recibir datos por la conexión serial.
-b. Reenviar por dicha conexión los datos recibidos cambiando las 
-mayúsculas por minúsculas y las minúsculas por mayúsculas.
+Hacer una aplicación de línea de comandos mediante el puerto serie del 
+microcontrolador.
+a. Al iniciar o enviar una h se debe mostrar el menu de ayuda con la 
+siguiente información:
+H o h (help): muestra este menu de ayuda.
+START o start: publica cada segundo un valor por el puerto serie
+STOP o stop: detiene la publicación del valor.
+T o t (time): El siguiente valor enviado cambiará el periodo de
+publicación de la señal entre 1 y 10 veces por segundo.
+M o m (mode): cambia el modo de fijo a alternativo. En modo fijo la 
+señal se emite mantiene siempre el mismo valor siendo el ultimo 
+emitido, por defecto debe ser un 1. Por otra parte, en modo 
+alternativo cada vez que se publica el valor se alterna entre 0 y 1
 */
